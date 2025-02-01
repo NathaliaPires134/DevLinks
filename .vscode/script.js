98ec76ad071699144347b7ac08b765946914d95f
NathaliaPires134 <snathalia22@gmail.com>
2025-02-01 18:04:11 -0300

    :tada: initial commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -3,12 +3,21 @@ function toggleMode() {
   html.classList.toggle("light")
   //pegar a tag img
   const img = document.querySelector("#profile img")
-  //substituir a img
+   //substituir a img
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a img light
     img.setAttribute("src", "./assets/AvatarLight.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/Avatar.png")
+  }
+
+  //substituir a img
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a img light
+    img.setAttribute("alt", "foto do botão de uma flor girassol")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("alt", "foto de uma flor girassol")
   }
 }
